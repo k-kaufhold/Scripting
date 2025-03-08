fca1117b46a74381141e42f0f95d8b8562928302
BufferTheHutt <net.exe@gmx.net>
2025-03-08 15:46:31 +0100

    scripts-update

---
+++ backup-script/documents-backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+document_dir=~/Dokumente
+backup_dir=~/Sync/Sync-Backup/ThinkPad/Dokumente
+mkdir -p $backup_dir
+day=$(date +'%y-%m-%d')
+echo "Backup file: $backup_dir/doc-backup-$day.tar.gz"
+tar czvf $backup_dir/doc-backup-$day.tar.gz -C $document_dir .
